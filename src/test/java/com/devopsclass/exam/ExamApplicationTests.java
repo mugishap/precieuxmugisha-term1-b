efa17354f79d9621a8d6cdc8e6fd3852b2f81620
mugishap <precieuxmugisha@gmail.com>
2023-12-18 08:57:17 +0200

    [init]: Initialize project

---
+++ src/test/java/com/devopsclass/exam/ExamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devopsclass.exam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
